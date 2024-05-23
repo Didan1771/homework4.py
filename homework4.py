69af813bc712a89f04516ff855a77df2530473a7
Didan1771 <didan1771@icloud.com>
2024-05-23 22:59:28 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,11 @@
+
+my_string = input()
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+no_spaces=my_string.replace(' ', '')
+print(no_spaces)
+first_symbol=my_string[0]
+print(first_symbol)
+last_symbol=my_string[-1]
+print(last_symbol)
